fd364ed492a752db8094cf27a35099eacabae2d8
John Ixcoy <jixcoy316@gmail.com>
2022-11-04 12:57:24 -0700

    Issue with merges

--- Project/Project/Program.cs
+++ Project/Project/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace Project
+﻿using System;
+
+namespace Project
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            
+            MergeSort m = new MergeSort();
+
+            int[] arr = { 1, 0, 2, 9, 3, 8, 4, 7, 5, 6 };
+
+            Console.WriteLine("Array before Merge Sort:");
+            foreach (int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            m.MergeS(arr);
+            watch.Stop();
+
+            Console.WriteLine("\nArray after Merge Sort:");
+            foreach (int s in arr)
+            {
+                Console.Write(s + " ");
+            }
         }
     }
 }
